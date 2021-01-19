611e4ab774635955791f6dde877e0608d944d433
jmutchek <jmutchek@microsoft.com>
2021-01-19 17:48:22 -0500

    wip

--- scripts/create-aks-cluster.ps1
+++ scripts/create-aks-cluster.ps1
@@ -1,3 +1,6 @@
 az akx get-versions --location northeurope
 az aks create -resource-group teamResources --name aks-oh-team-4 --node-count 2 --kubernetes-version 1.19.6 --location northeurope --generate-ssh-keys
 az aks get-credentials --resource-group teamResources --name aks-oh-team-4
+
+# connect AKS to ACR (so it can pull images)
+az aks update --resource-group teamResource --name aks-oh-team-4 --attach-acr registrykna5332
--- scripts/kubectl-cmds.ps1
+++ scripts/kubectl-cmds.ps1
@@ -2,3 +2,7 @@ kubectl get nodes
 kubectl get namespaces
 kubectl describe node aks-nodepool1-35270789-vmss000000
 kubectl create namespace teamsviewer
+
+# view the manifest for an existing deployment
+kubectl get deployments/coredns -n kube-system -o yaml
+
