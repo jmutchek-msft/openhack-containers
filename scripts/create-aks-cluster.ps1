600ea5f29a6515a86f538ae0fc14783affd99a87
jmutchek <jmutchek@microsoft.com>
2021-01-19 17:17:33 -0500

    add get-creds

--- scripts/create-aks-cluster.ps1
+++ scripts/create-aks-cluster.ps1
@@ -1,2 +1,3 @@
 az akx get-versions --location northeurope
 az aks create -resource-group teamResources --name aks-oh-team-4 --node-count 2 --kubernetes-version 1.19.6 --location northeurope --generate-ssh-keys
+az aks get-credentials --resource-group teamResources --name aks-oh-team-4
