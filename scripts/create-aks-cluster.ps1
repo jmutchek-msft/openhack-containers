9da617e869f482df69ebc19a48b63383cdb12fb9
jmutchek <jmutchek@microsoft.com>
2021-01-19 17:01:17 -0500

    add create-aks

---
+++ scripts/create-aks-cluster.ps1
@@ -0,0 +1,2 @@
+az akx get-versions --location northeurope
+az aks create -resource-group teamResources --name aks-oh-team-4 --node-count 2 --kubernetes-version 1.19.6 --location northeurope --generate-ssh-keys
