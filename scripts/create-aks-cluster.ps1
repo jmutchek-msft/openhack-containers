c932ce37bb285734260474541c29bf7b620f3d97
jmutchek <jmutchek@microsoft.com>
2021-01-19 17:52:07 -0500

    comments

--- scripts/create-aks-cluster.ps1
+++ scripts/create-aks-cluster.ps1
@@ -1,5 +1,10 @@
-az akx get-versions --location northeurope
+# retrieve the supported versions of AKS
+az aks get-versions --location northeurope
+
+# create the AKS cluster
 az aks create -resource-group teamResources --name aks-oh-team-4 --node-count 2 --kubernetes-version 1.19.6 --location northeurope --generate-ssh-keys
+
+# grab admin creds so we can use kubectl commands locally
 az aks get-credentials --resource-group teamResources --name aks-oh-team-4
 
 # connect AKS to ACR (so it can pull images)
