60899e190698c13dec99bdd46c4fc869bb102e7a
jungho <junghokim@microsoft.com>
2021-01-20 15:17:16 -0500

    added tripviewer

---
+++ src/tripviewer/yaml/deploy-tripviewer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#create the namespace first
+namespace=tripviewer
+
+kubectl create --namespace="$namespace" -f configMap.yaml
+kubectl create --namespace="$namespace" -f deployment.yaml
+kubectl create --namespace="$namespace" -f service.yaml
