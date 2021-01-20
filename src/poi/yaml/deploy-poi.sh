394b556209b6b34126c5afd4c0e5f642cc784240
jungho <junghokim@microsoft.com>
2021-01-20 08:57:50 -0500

    poi service secrets

--- src/poi/yaml/deploy-poi.sh
+++ src/poi/yaml/deploy-poi.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-
-namespace=tripviewer
 #create the namespace first
+namespace=tripviewer
+
+#create generic secret from file
+kubectl create secret generic sql-secrets --namespace="$namespace" \
+--from-file=SQL_PASSWORD=./secrets/password \
+--from-file=SQL_USER=./secrets/user
+
 kubectl create --namespace="$namespace" -f configMap.yaml
-kubectl create --namespace="$namespace" -f secrets.yaml
 kubectl create --namespace="$namespace" -f deployment.yaml
 kubectl create --namespace="$namespace" -f service.yaml
