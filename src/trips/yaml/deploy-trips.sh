59f1251805d8638b208f95bb244e325a91bb63a4
JeffDeadwyler <jeff.deadwyler@optimalcloud.com>
2021-01-20 13:56:41 -0500

    Update to trips service

---
+++ src/trips/yaml/deploy-trips.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#create the namespace first
+namespace=tripviewer
+
+#create generic secret from file
+#kubectl create secret generic sql-secrets --namespace="$namespace" \
+#--from-file=SQL_PASSWORD=./secrets/password \
+#--from-file=SQL_USER=./secrets/user
+
+kubectl create --namespace="$namespace" -f configMap.yaml
+kubectl create --namespace="$namespace" -f deployment.yaml
+kubectl create --namespace="$namespace" -f service.yaml
