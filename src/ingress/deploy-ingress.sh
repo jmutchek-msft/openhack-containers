a6a9c9ed424b08a1a691b5fcb5b1531187e9fbd2
jungho <junghokim@microsoft.com>
2021-01-21 13:18:14 -0500

    fixed routing issues from tripviewer

--- src/ingress/deploy-ingress.sh
+++ src/ingress/deploy-ingress.sh
@@ -3,13 +3,15 @@
 
 
 # Create a namespace for your ingress resources
-kubectl create namespace ingress
+namespace=jungho-ingress
 
 # Use Helm to deploy an NGINX ingress controller
 helm install nginx-ingress ingress-nginx/ingress-nginx \
-    --namespace ingress \
+    --namespace $namespace \
     --set controller.replicaCount=2 \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
 
+kubectl apply -f ingress-apis.yaml -n jungho-apis
+kubectl apply -f ingress-web.yaml -n jungho-web
