06a934ad7a5dd87815d44c7002d8bfedd49b0137
jmutchek-msft <jmutchek@microsoft.com>
2021-01-20 19:11:18 -0500

    wip rbac

--- scripts/assign-permissions.sh
+++ scripts/assign-permissions.sh
@@ -2,10 +2,11 @@
 
 RG=teamResources
 AKS=team4-openhack
-
 AKS_ADMIN_GROUP=myAKSAdminGroup
 WEB_GROUP=webRoleGroup
 API_GROUP=apiRoleGroup
+WEBDEV=webdev@OTAPRD360ops.onmicrosoft.com
+APIDEV=apidev@OTAPRD360ops.onmicrosoft.com
 
 HACKER1=hacker13h3@OTAPRD360ops.onmicrosoft.com
 HACKER2=hacker22l7@OTAPRD360ops.onmicrosoft.com
@@ -17,13 +18,24 @@ HACKER6=hacker6re8@OTAPRD360ops.onmicrosoft.com
 AKS_ID=$(az aks show --resource-group $RG --name $AKS --query id -o tsv)
 
 # Create an Azure AD groups
-ADMIN_ID=$(az ad group create --display-name $AKS_ADMIN_GROUP --mail-nickname $AKS_ADMIN_GROUP)
-WEB_ID=$(az ad group create --display-name $WEB_GROUP --mail-nickname $WEB_GROUP)
-API_ID=$(az ad group create --display-name $API_GROUP --mail-nickname $API_GROUP)
+# ADMIN_ID=$(az ad group create --display-name $AKS_ADMIN_GROUP --mail-nickname $AKS_ADMIN_GROUP)
+WEB_ID=$(az ad group create --display-name $WEB_GROUP --mail-nickname $WEB_GROUP --query objectId -o tsv)
+API_ID=$(az ad group create --display-name $API_GROUP --mail-nickname $API_GROUP --query objectId -o tsv)
 
 # Role assignment
-az role assignment create --assignee $WEB_ID --role "Azure Kubernetes Service Cluster User Role" --scope $AKS_ID
-az role assignment create --assignee $API_ID --role "Azure Kubernetes Service Cluster User Role" --scope $AKS_ID
+az role assignment create --assignee $WEB_ID --role 'Azure Kubernetes Service Cluster User Role' --scope $AKS_ID
+az role assignment create --assignee $API_ID --role 'Azure Kubernetes Service Cluster User Role' --scope $AKS_ID
+
+## API-DEV
+ACCOUNT_ID=$(az ad user show --id $APIDEV --query objectId -o tsv)
+az ad group member add --group $API_GROUP --member-id $ACCOUNT_ID
+
+## WEB-DEV
+ACCOUNT_ID=$(az ad user show --id $WEBDEV --query objectId -o tsv)
+az ad group member add --group $WEB_GROUP --member-id $ACCOUNT_ID
+
+
+
 
 ## HACKER 1
 # Get the account credentials for the logged in user
